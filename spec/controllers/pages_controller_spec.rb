b69cd2ede3c345c1438348dab241c6b8fe28eb78
anthony <anthony.lazzaro@unrulymedia.com>
2012-01-20 17:13:58 +0000

    Refactored views

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,25 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+                      :content => "Home")
+  end
+  
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+                      :content => "Contact")
+  end
+
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+                      :content => "About")
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
